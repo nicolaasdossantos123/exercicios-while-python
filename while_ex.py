2fb2963a897c5f5aa5e8d1561bc07e9d34f70ac9
Nicolaas dos Santos <nicolaasdossantos123@gmail.com>
2026-02-21 16:56:14 -0300

    Add files via upload

---
+++ while_ex.py
@@ -0,0 +1,11 @@
+soma = 0
+numero = -1
+
+while numero != 0:
+     numero = int(input("Digite seu número (0 para sair): "))
+      
+     if numero != 0:
+        soma = soma + numero
+
+
+print("Soma total", soma)
